eb68e30e3dd84da8ad8f0f702eb1ee835e363690
Hunter Straub <hunterstraub@yahoo.com>
2016-02-27 17:34:30 -0800

    Update shape.h

--- shape.h
+++ shape.h
@@ -1,50 +1,25 @@
 /* ----------------------------------------------------------------------------
- * Copyright &copy; 2016 Ben Blazak <bblazak@fullerton.edu>
- * Released under the [MIT License] (http://opensource.org/licenses/MIT)
- * ------------------------------------------------------------------------- */
-
-/**
- * Class representing a shape (triangle or square) with biases about what kinds
- * of shapes it wants for neighbors.
- */
+* Copyright &copy; 2016 Hunter Straub <Hunterstraub@csu.fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
 
 #ifndef SHAPE_H
 #define SHAPE_H
 // ----------------------------------------------------------------------------
 
 #include <string>
-
 #include "buffer.h"
 #include "neighborhood.h"
 
 // ----------------------------------------------------------------------------
 
 class Neighborhood;
-/**
- * Forward declare `Neighborhood`, in case this file gets included first.
- *
- * This is definitely not the cleanest way to do things; but it seems to me
- * right now to be the simplest.
- */
-
+/
 // ----------------------------------------------------------------------------
 
 class Shape {
     private:
         std::string type_;
-        /**
-         * A string representing the "type" of the `Shape` (either "empty",
-         * "triangle", or "square").
-         *
-         * Notes:
-         * - It would be more "object oriented" to have separate `Empty`,
-         *   `Triangle`, and `Square` types derived from this class.  But I
-         *   decided that the complexity that introduced to the design wasn't
-         *   worth it for this project.  Also, it's nice to try this method
-         *   out, since it is a completely valid way to do things, and
-         *   sometimes it turns out to be the best.  And don't worry: we'll get
-         *   plenty of practice with inheritance later on.
-         */
 
         std::string type_art_;
         /**
@@ -72,7 +47,6 @@ class Shape {
          * rows).
          */
 
-
     public:
         static const unsigned int size_x = 5;  // must match actual size
         static const unsigned int size_y = 3;  // must match actual size
@@ -83,9 +57,7 @@ class Shape {
 		setType(type);
 	}
 
-
     ~Shape() = default;
-
 
     std::string getType() { return type_; }
 
